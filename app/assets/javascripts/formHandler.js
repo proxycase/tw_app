1c9d5c025a531556f33c9265b0fbef5ef446e212
Hugh Sato <hughsato@gmail.com>
2015-08-18 22:22:45 -0500

    Number Checking Functionality Added

--- app/assets/javascripts/formHandler.js
+++ app/assets/javascripts/formHandler.js
@@ -1,6 +1,4 @@
-jQuery(function($){
-   $("#phone").mask("(999) 999-9999");
-});
+
 
 
 // // keypress
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( jquery-2.1.0.min.js bootstrap.js blocs.js jqBootstrapValidation.js formHandler.js jquery.maskedinput.min.js)
-Rails.application.config.assets.precompile += %w( bootstrap.css style.scss font-awesome.min.css dots-bg.png thumbs-up.gif)
+Rails.application.config.assets.precompile += %w( jquery-2.1.0.min.js bootstrap.js blocs.js formHandler.js jquery.maskedinput.min.js)
+Rails.application.config.assets.precompile += %w( bootstrap.css style.scss font-awesome.min.css dots-bg.png)
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
